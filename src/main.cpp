20390607dc50ba1ca18a274241ba726f7cee8ce5
Liam EIme <liameime@outlook.com>
2025-11-09 21:25:46 +1000

    Update trigger threshold for lenses to 1.72V

--- src/main.cpp
+++ src/main.cpp
@@ -29,9 +29,8 @@ int main() {
     adc_sampler_init(BOARD_ADC_GPIO, sample_rate, buf_a, buf_b, 5000);
 
     // Event logger configuration
-    // const float trigger_threshold_v = 0.9f; // 0.9 V for array receiver
     // const float trigger_threshold_v = 0.2f; // 0.2 V  for single receiver
-    const float trigger_threshold_v = 1.5f; // 1.5 V  for lenses
+    const float trigger_threshold_v = 1.72f; // 1.72 V  for lenses
     const float event_duration_s    = 0.5f;  // 0.5 second
     EventLogger::init(trigger_threshold_v, event_duration_s, 3.3f, "event");
     // Enable lightweight debug summaries to help tune thresholds
